82aafaca61794804e694b5385601f952c8e6fe0c
Sanskar-12 <sanskarv2004@gmail.com>
2025-06-11 17:55:53 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <Button>Hello</Button>
+      <p className="text-rose-400">Hello</p>
+    </div>
+  );
+};
+
+export default Home;
