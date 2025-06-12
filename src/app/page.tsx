51302826bb899b337990e12bf5090df10fbc4617
Sanskar-12 <sanskarv2004@gmail.com>
2025-06-12 12:26:48 +0530

    modified classed for Textarea and Progress bar and added checkbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 const Home = () => {
   return (
-    <div>
-      <Button variant={"elevated"}>Hello</Button>
-      <Input placeholder="Input" />
-      <p className="text-rose-400">Hello</p>
+    <div className="flex flex-col p-4 gap-y-4">
+      <div>
+        <Button variant={"elevated"}>Hello</Button>
+      </div>
+      <div>
+        <Input placeholder="Input" />
+      </div>
+      <div>
+        <Progress value={30} />
+      </div>
+      <div>
+        <Textarea placeholder="Textarea" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
 };
