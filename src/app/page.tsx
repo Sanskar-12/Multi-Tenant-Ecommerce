b9a7f0806def57e617e14a38ec121676d0117c4a
Sanskar-12 <sanskarv2004@gmail.com>
2025-06-12 12:12:40 +0530

    modified classed for Button and Input to make it neobrutal style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const Home = () => {
   return (
     <div>
-      <Button>Hello</Button>
+      <Button variant={"elevated"}>Hello</Button>
+      <Input placeholder="Input" />
       <p className="text-rose-400">Hello</p>
     </div>
   );
